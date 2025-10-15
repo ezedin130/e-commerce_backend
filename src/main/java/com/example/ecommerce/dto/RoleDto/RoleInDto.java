5b1ab5e28b268b8ee17be9760b91b612370e6ef1
Ezedi <ezedinawol130@gmail.com>
2025-10-16 00:13:20 +0300

    data transfer object(dto) for role

---
+++ src/main/java/com/example/ecommerce/dto/RoleDto/RoleInDto.java
@@ -0,0 +1,8 @@
+package com.example.ecommerce.dto.RoleDto;
+
+import lombok.Data;
+
+@Data
+public class RoleInDto {
+    private String name;
+}
---
+++ src/main/java/com/example/ecommerce/dto/RoleDto/RoleOutDto.java
@@ -0,0 +1,9 @@
+package com.example.ecommerce.dto.RoleDto;
+
+import lombok.Data;
+
+@Data
+public class RoleOutDto {
+    private Long id;
+    private String name;
+}
