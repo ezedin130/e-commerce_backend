f3231c393d881fbccb3c70f3c2164a1b213f986c
Ezedi <ezedinawol130@gmail.com>
2025-10-15 22:34:05 +0300

    data transfer object(dto) for order-item

---
+++ src/main/java/com/example/ecommerce/dto/OrderItemDto/OrderItemInDto.java
@@ -0,0 +1,12 @@
+package com.example.ecommerce.dto.OrderItemDto;
+
+import lombok.Data;
+
+@Data
+public class OrderItemInDto {
+    private Integer quantity;
+    private double unitPrice;
+    private double subTotalPrice;
+    private Long orderId;
+    private Long productVariantId;
+}
---
+++ src/main/java/com/example/ecommerce/dto/OrderItemDto/OrderItemOutDto.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce.dto.OrderItemDto;
+
+import lombok.Data;
+
+@Data
+public class OrderItemOutDto {
+    private Long id;
+    private Integer quantity;
+    private double unitPrice;
+    private double subTotalPrice;
+    private Long orderId;
+    private Long productVariantId;
+}
