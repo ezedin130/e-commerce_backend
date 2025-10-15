00e54f6c54f580f26f1ca7e2cfaeece38d5d1c25
Ezedi <ezedinawol130@gmail.com>
2025-10-16 00:12:04 +0300

    mapper used to map from product-variant dto to product-variant entity and vice versa

---
+++ src/main/java/com/example/ecommerce/mapper/ProductVariantMapper.java
@@ -0,0 +1,28 @@
+package com.example.ecommerce.mapper;
+
+import com.example.ecommerce.dto.ProductDto.ProductInDto;
+import com.example.ecommerce.dto.ProductVariantDto.ProductVariantInDto;
+import com.example.ecommerce.dto.ProductVariantDto.ProductVariantOutDto;
+import com.example.ecommerce.model.Product;
+import com.example.ecommerce.model.ProductVariant;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductVariantMapper {
+    public ProductVariant toEntity(ProductVariantInDto dto, Product product){
+        return ProductVariant.builder()
+                .size(dto.getSize())
+                .color(dto.getColor())
+                .product(product)
+                .build();
+    }
+    public ProductVariantOutDto toDto(ProductVariant productVariant){
+        //ToDo: make sure the @transient fields are applicable when needed
+        ProductVariantOutDto dto = new ProductVariantOutDto();
+        dto.setId(productVariant.getId());
+        dto.setSize(productVariant.getSize());
+        dto.setColor(productVariant.getColor());
+        dto.setProductId(productVariant.getProduct().getId());
+        return dto;
+    }
+}
