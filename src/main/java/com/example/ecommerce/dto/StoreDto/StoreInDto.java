3912b6203e1879107b30dc0a74a099ad113fb2c9
Ezedi <ezedinawol130@gmail.com>
2025-10-16 00:13:34 +0300

    data transfer object(dto) for store

---
+++ src/main/java/com/example/ecommerce/dto/StoreDto/StoreInDto.java
@@ -0,0 +1,9 @@
+package com.example.ecommerce.dto.StoreDto;
+
+import lombok.Data;
+
+@Data
+public class StoreInDto {
+    private String name;
+    private String address;
+}
---
+++ src/main/java/com/example/ecommerce/dto/StoreDto/StoreOutDto.java
@@ -0,0 +1,10 @@
+package com.example.ecommerce.dto.StoreDto;
+
+import lombok.Data;
+
+@Data
+public class StoreOutDto {
+    private Long id;
+    private String name;
+    private String address;
+}
