f95e095b7d574887e6f5fa00f2678f0630e9852b
Ezedi <ezedinawol130@gmail.com>
2025-10-29 22:19:09 +0300

    creates order-item automatically by calling createOrderItemForOrder from OrderItemService

--- src/main/java/com/example/ecommerce/service/OrderService.java
+++ src/main/java/com/example/ecommerce/service/OrderService.java
@@ -4,8 +4,11 @@ import com.example.ecommerce.dto.OrderDto.OrderInDto;
 import com.example.ecommerce.dto.OrderDto.OrderOutDto;
 import com.example.ecommerce.mapper.OrderMapper;
 import com.example.ecommerce.model.Order;
+import com.example.ecommerce.model.ProductVariant;
 import com.example.ecommerce.repository.OrderRepository;
+import com.example.ecommerce.repository.ProductVariantRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,11 +17,20 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class OrderService {
+    @Autowired
     private final OrderRepository orderRepo;
+    @Autowired
     private final OrderMapper mapper;
+    @Autowired
+    private final OrderItemService itemService;
+    @Autowired
+    private final ProductVariantRepository variantRepo;
     public OrderOutDto createOrder(OrderInDto dto){
-        Order order = mapper.toEntity(dto);
+        ProductVariant variant = variantRepo.findById(dto.getProductVariantId())
+                .orElseThrow(()-> new RuntimeException("Product Variant Not Found"));
+        Order order = mapper.toEntity(dto,variant);
         Order savedOrder = orderRepo.save(order);
+        itemService.createOrderItemForOrder(variant.getId(),savedOrder);
         return mapper.toDto(savedOrder);
     }
     public  Order getOrderById(Long id){
