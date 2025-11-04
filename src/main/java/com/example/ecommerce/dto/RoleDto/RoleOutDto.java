f3f9c17652c1252cb73713430c5390c83f9a4975
Ezedi <ezedinawol130@gmail.com>
2025-11-05 00:25:47 +0300

    no argument constructor and all arguments constructor added

--- src/main/java/com/example/ecommerce/dto/RoleDto/RoleOutDto.java
+++ src/main/java/com/example/ecommerce/dto/RoleDto/RoleOutDto.java
@@ -1,8 +1,12 @@
 package com.example.ecommerce.dto.RoleDto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RoleOutDto {
     private Long id;
     private String name;
