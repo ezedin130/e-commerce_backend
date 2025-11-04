39b5d2abd21a42b82755cb14cc96a5b46b40714e
Ezedi <ezedinawol130@gmail.com>
2025-11-05 00:27:36 +0300

    role-Id added to accept role from the user

--- src/main/java/com/example/ecommerce/dto/UserDto/UserInDto.java
+++ src/main/java/com/example/ecommerce/dto/UserDto/UserInDto.java
@@ -2,9 +2,13 @@ package com.example.ecommerce.dto.UserDto;
 
 import lombok.Data;
 
+import java.util.Set;
+
 @Data
 public class UserInDto {
     private String firstName;
     private String lastName;
+    private String userName;
     private String password;
+    private Set<Long> roleIds;
 }
