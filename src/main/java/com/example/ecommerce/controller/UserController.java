15d4b0de92ea64e7e565bed32d4e63b96b9dc299
Ezedi <ezedinawol130@gmail.com>
2025-11-08 01:43:11 +0300

    implement user authentication endpoint

--- src/main/java/com/example/ecommerce/controller/UserController.java
+++ src/main/java/com/example/ecommerce/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.example.ecommerce.controller;
 
+import com.example.ecommerce.dto.AuthResponseDto;
 import com.example.ecommerce.dto.UserDto.UserInDto;
+import com.example.ecommerce.dto.UserDto.UserLoginDto;
 import com.example.ecommerce.dto.UserDto.UserOutDto;
 import com.example.ecommerce.mapper.UserMapper;
 import com.example.ecommerce.model.User;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/user")
 @Validated
 @RequiredArgsConstructor
 public class UserController {
@@ -22,10 +25,14 @@ public class UserController {
     @Autowired
     private final UserMapper userMapper;
 
-    @PostMapping("/user")
+    @PostMapping("/create-user")
     public ResponseEntity<UserOutDto> createUser(@RequestBody UserInDto dto){
         UserOutDto result = userService.createUser(dto);
         return ResponseEntity.ok(result);
+    }
+    @PostMapping("/login")
+    public AuthResponseDto login(@RequestBody UserLoginDto dto){
+        return userService.login(dto);
     }
     @GetMapping("/get-user-by-id/{id}")
     public ResponseEntity<UserOutDto> getUserById(@PathVariable Long id){
@@ -33,7 +40,7 @@ public class UserController {
         UserOutDto dto = userMapper.toDto(user);
         return ResponseEntity.ok(dto);
     }
-    @GetMapping("/user")
+    @GetMapping()
     public ResponseEntity<List<UserOutDto>> getAllUsers(){
         List<UserOutDto> result = userService.findAllUsers();
         return ResponseEntity.ok(result);
