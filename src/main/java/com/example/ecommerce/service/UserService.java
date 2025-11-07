376a257522457690a8ca1b84aa05601e2f7421c9
Ezedi <ezedinawol130@gmail.com>
2025-11-08 01:46:39 +0300

    add user login authentication service

--- src/main/java/com/example/ecommerce/service/UserService.java
+++ src/main/java/com/example/ecommerce/service/UserService.java
@@ -1,15 +1,24 @@
 package com.example.ecommerce.service;
 
+import com.example.ecommerce.dto.AuthResponseDto;
 import com.example.ecommerce.dto.UserDto.UserInDto;
+import com.example.ecommerce.dto.UserDto.UserLoginDto;
 import com.example.ecommerce.dto.UserDto.UserOutDto;
 import com.example.ecommerce.mapper.UserMapper;
+import com.example.ecommerce.model.Role;
 import com.example.ecommerce.model.User;
 import com.example.ecommerce.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
@@ -20,10 +29,39 @@ public class UserService {
     @Autowired
     private final UserRepository userRepo;
 
+    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+    @Autowired
+    private final AuthenticationManager authManager;
+    @Autowired
+    private final JwtService jwtService;
+
     public UserOutDto createUser(UserInDto dto){
         User user = mapper.toEntity(dto);
+        user.setPassword(encoder.encode(user.getPassword()));
         User savedUser = userRepo.save(user);
         return mapper.toDto(savedUser);
+    }
+    public AuthResponseDto login(UserLoginDto dto){
+        Authentication auth = authManager.authenticate(
+          new UsernamePasswordAuthenticationToken(
+                  dto.getUserName(),
+                  dto.getPassword())
+        );
+        if(auth.isAuthenticated()){
+            UserDetails userDetails = (UserDetails) auth.getPrincipal();
+            String token = jwtService.generateToken(dto.getUserName());
+
+            User user = userRepo.findByUsername(dto.getUserName());
+            Set<String> roleNames = user.getRoles().stream()
+                    .map(Role::getName)
+                    .collect(Collectors.toSet());
+            return new AuthResponseDto(
+                    token,
+                    user.getUsername(),
+                    roleNames
+            );
+        }
+        return null;
     }
     public User findUserById(Long id){
        return userRepo.findById(id)
