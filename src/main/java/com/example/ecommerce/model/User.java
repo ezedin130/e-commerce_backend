f147b77f49edeb6351f51dd7343d20289f20c389
Ezedi <ezedinawol130@gmail.com>
2025-11-08 01:42:12 +0300

    optimize entity loading with lazy fetching

--- src/main/java/com/example/ecommerce/model/User.java
+++ src/main/java/com/example/ecommerce/model/User.java
@@ -30,7 +30,7 @@ public class User {
     //TODO: username is supposed to be generated automatically when user registers
     @NotBlank(message = "password is required")
     private String password;
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(
             name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id"),
