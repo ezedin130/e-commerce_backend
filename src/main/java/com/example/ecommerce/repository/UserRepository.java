f0adbe5ba8e04104c3332a05064050e14c7c570d
Ezedi <ezedinawol130@gmail.com>
2025-11-08 01:45:54 +0300

    resolve lazy loading issues with JOIN FETCH query

--- src/main/java/com/example/ecommerce/repository/UserRepository.java
+++ src/main/java/com/example/ecommerce/repository/UserRepository.java
@@ -2,9 +2,17 @@ package com.example.ecommerce.repository;
 
 import com.example.ecommerce.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+    @Query("SELECT DISTINCT u FROM User u LEFT JOIN FETCH u.roles WHERE u.username = :username")
+    Optional<User> findByUsernameWithRoles(@Param("username") String username);
+
     User findByUsername(String userName);
 }
