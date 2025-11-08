f8079ae90a230258d0ac4eb05cf37b8df615d9d5
Ezedi <ezedinawol130@gmail.com>
2025-11-08 20:12:38 +0300

    add password management endpoints with role-based access control

--- src/main/java/com/example/ecommerce/controller/UserController.java
+++ src/main/java/com/example/ecommerce/controller/UserController.java
@@ -4,12 +4,15 @@ import com.example.ecommerce.dto.AuthResponseDto;
 import com.example.ecommerce.dto.UserDto.UserInDto;
 import com.example.ecommerce.dto.UserDto.UserLoginDto;
 import com.example.ecommerce.dto.UserDto.UserOutDto;
+import com.example.ecommerce.dto.UserDto.UserPasswordChangeDto;
 import com.example.ecommerce.mapper.UserMapper;
 import com.example.ecommerce.model.User;
 import com.example.ecommerce.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -40,9 +43,25 @@ public class UserController {
         UserOutDto dto = userMapper.toDto(user);
         return ResponseEntity.ok(dto);
     }
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping()
     public ResponseEntity<List<UserOutDto>> getAllUsers(){
         List<UserOutDto> result = userService.findAllUsers();
         return ResponseEntity.ok(result);
+    }
+    @PutMapping("/change-password")
+    public ResponseEntity<String> changeOwnPassword(@RequestBody UserPasswordChangeDto dto){
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        userService.changeOwnPassword(username,dto.getCurrentPassword(),dto.getNewPassword());
+        return ResponseEntity.ok("Password changed successfully");
+    }
+    @PreAuthorize("hasRole('ADMIN')")
+    @PutMapping("/{id}/reset-password")
+    public ResponseEntity<String> resetUserPassword(
+            @PathVariable Long id,
+            @RequestBody UserPasswordChangeDto dto
+    ) {
+        userService.resetUserPassword(id, dto.getNewPassword());
+        return ResponseEntity.ok("Password reset successfully");
     }
 }
