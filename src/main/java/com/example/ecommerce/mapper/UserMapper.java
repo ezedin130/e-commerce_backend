5437844088be874da8dd1b444391f55e03cf2b07
Ezedi <ezedinawol130@gmail.com>
2025-11-08 20:27:28 +0300

    username and password removed

--- src/main/java/com/example/ecommerce/mapper/UserMapper.java
+++ src/main/java/com/example/ecommerce/mapper/UserMapper.java
@@ -30,8 +30,6 @@ public class UserMapper {
         return User.builder()
                 .firstName(dto.getFirstName())
                 .lastName(dto.getLastName())
-                .password(dto.getPassword())
-                .username(dto.getUserName())
                 .roles(roles)
                 .build();
     }
