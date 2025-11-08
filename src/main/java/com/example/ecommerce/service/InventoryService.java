fe8c7832ff9a55f4351ac81890dda4b6bc4dd7bb
Ezedi <ezedinawol130@gmail.com>
2025-11-09 01:36:51 +0300

    method for updating inventory

--- src/main/java/com/example/ecommerce/service/InventoryService.java
+++ src/main/java/com/example/ecommerce/service/InventoryService.java
@@ -6,9 +6,11 @@ import com.example.ecommerce.mapper.InventoryMapper;
 import com.example.ecommerce.model.Inventory;
 import com.example.ecommerce.model.ProductVariant;
 import com.example.ecommerce.model.Store;
+import com.example.ecommerce.model.User;
 import com.example.ecommerce.repository.InventoryRepository;
 import com.example.ecommerce.repository.ProductVariantRepository;
 import com.example.ecommerce.repository.StoreRepository;
+import com.example.ecommerce.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +30,8 @@ public class InventoryService {
     private final StoreRepository storeRepo;
     @Autowired
     private final InventoryMapper mapper;
+    @Autowired
+    private final UserRepository userRepo;
     public InventoryOutDto createInventory(InventoryInDto dto){
         ProductVariant variant = variantRepo.findById(dto.getProductVariantId())
                 .orElseThrow(() -> new RuntimeException("product variant not found"));
@@ -60,5 +64,20 @@ public class InventoryService {
         return inventoryRepo.findAll().stream()
                 .map(mapper::toDto)
                 .collect(Collectors.toList());
+    }
+    public InventoryOutDto updateInventory(Long id, int quantity, String username){
+        User currentUser = userRepo.findByUsername(username);
+        if(currentUser == null) {
+            throw new RuntimeException("user not found");
+        }
+        Inventory inventory = inventoryRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("inventory not found"));
+        User owner = inventory.getStore().getUser();
+        if(!owner.getId().equals(currentUser.getId())){
+            throw new RuntimeException("Unauthorized: You can only update order items for your own order");
+        }
+        inventory.setQuantity(quantity);
+        Inventory savedInventory = inventoryRepo.save(inventory);
+        return mapper.toDto(savedInventory);
     }
 }
