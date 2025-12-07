29455ad3b5c7f9ce852fb12f6f152addd8c4afb6
Ezedin Awol <140709867+ezedin130@users.noreply.github.com>
2025-12-07 16:28:08 +0300

    method to find user by id that is optional

--- src/main/java/com/example/ecommerce/repository/StoreRepository.java
+++ src/main/java/com/example/ecommerce/repository/StoreRepository.java
@@ -4,6 +4,9 @@ import com.example.ecommerce.model.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StoreRepository extends JpaRepository<Store,Long> {
+    Optional<Store> findByUserId(Long userId);
 }
