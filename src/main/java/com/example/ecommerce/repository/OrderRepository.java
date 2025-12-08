549f3277c184ee748dc7c3196ecb993c8b265ba1
Ezedin Awol <140709867+ezedin130@users.noreply.github.com>
2025-12-08 16:20:32 +0300

    implement query to get orders associated with a store owner

--- src/main/java/com/example/ecommerce/repository/OrderRepository.java
+++ src/main/java/com/example/ecommerce/repository/OrderRepository.java
@@ -2,8 +2,13 @@ package com.example.ecommerce.repository;
 
 import com.example.ecommerce.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order,Long> {
+    @Query("SELECT o FROM Order o WHERE o.productVariant.product.store.user.id = :ownerId")
+    List<Order> findOrdersForStoreOwner(Long ownerId);
 }
